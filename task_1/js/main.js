fe5b89b9d7faf71569f9e92f4ff9dec8ad9b5d32
Collins Kipruto <collinstunge005@gmail.com>
2025-07-14 21:35:10 +0300

    submit

---
+++ task_1/js/main.js
@@ -0,0 +1,8 @@
+var teacher3 = {
+    firstName: "Mary",
+    fullTimeEmployee: false,
+    lastName: "Smith",
+    location: "Saul",
+    contract: true
+};
+console.log(teacher3);
---
+++ task_1/js/main.ts
@@ -0,0 +1,15 @@
+interface Teacher{
+    firstName: string;
+    fullTimeEmployee: boolean;
+    lastName: string;
+    location: string;
+    contract: boolean;
+}
+
+const teacher3 : Teacher={
+    firstName: "Mary",
+    fullTimeEmployee: false,
+    lastName: "Smith",
+    location: "Saul",
+    contract: true
+};
